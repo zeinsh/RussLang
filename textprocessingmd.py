4ec27180bc3570a97ba977af1f7f1424948198e3
zeinsh <zeineshahine@gmail.com>
2017-06-09 23:29:29 +0300

    add textprocessingmd

--- textprocessingmd.py
+++ textprocessingmd.py
@@ -5,3 +5,16 @@ def getWordList(text):
     words = nltk.word_tokenize(text)
     ret=[w for w in words if len(w)>2]
     return ret
+
+
+def decodeText(text):
+    try:
+         return str(text).decode('utf8')
+    except:
+         return text
+
+def encodeText(text):
+    try:
+         return text.encode('utf8')
+    except:
+         return text
